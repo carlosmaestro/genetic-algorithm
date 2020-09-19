1d6f1193b0b9bd3b6c05d5dd44ea2cfe2486c361
Carlos Maestro <chmmaestro@gmail.com>
2020-09-19 14:40:24 -0300

    update ts build config

--- out/start.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var population_1 = require("./src/population");
-console.log('iniciando algoritmo..');
-var population = new population_1.Population(5, 3);
-console.log(population);
-console.log(population);
-//# sourceMappingURL=start.js.map
